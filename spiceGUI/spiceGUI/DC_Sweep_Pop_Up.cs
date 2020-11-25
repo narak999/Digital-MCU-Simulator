8c03cd9ba9d58a61b66afb0a65cc70f9a3187f75
j127009202 <75005779+j127009202@users.noreply.github.com>
2020-11-25 00:00:25 -0600

    Add files via upload

---
+++ spiceGUI/spiceGUI/DC_Sweep_Pop_Up.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spiceGUI
+{
+    public partial class DC_Sweep_Pop_Up : Form
+    {
+        ListBox listbox;
+        List<string> component;
+        List<string> node;
+        public DC_Sweep_Pop_Up(ListBox _listbox, List<string> _component, List<string> _node)
+        {
+            InitializeComponent();
+            listbox = _listbox;
+            component = _component;
+            node = _node;
+            comboBox1.DataSource = component;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string comp = comboBox1.GetItemText(comboBox1.SelectedItem);
+            string VStart = textBox1.Text;
+            string VStop = textBox2.Text;
+            string Vincr = textBox3.Text;
+            listbox.Items.Add("dc " + comp + "  " + VStart + " " + VStop + " " + Vincr + "\n");
+        }
+    }
+}
---
+++ spiceGUI/spiceGUI/Edit_Source_Item_Pop_Up.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spiceGUI
+{
+    public partial class Edit_Source_Item_Pop_Up : Form
+    {
+        ListBox listbox;
+        List<string> component;
+        List<string> node;
+        public Edit_Source_Item_Pop_Up(ListBox _listbox, List<string> _component, List<string> _node)
+        {
+            InitializeComponent();
+            listbox = _listbox;
+            component = _component;
+            node = _node;
+            int index = listbox.SelectedIndex;
+            textBox1.Text = listbox.Items[index].ToString();
+        }
+
+        private void EditSourceItem_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int index = listbox.SelectedIndex;
+            listbox.Items.RemoveAt(index);
+            listbox.Items.Insert(index, textBox1.Text);
+        }
+    }
+}
