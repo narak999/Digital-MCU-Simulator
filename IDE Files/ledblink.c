08eb9279c0875282c10751c17f50e94db4afaa6e
narak999 <bunnaraktheng@gmail.com>
2021-04-17 13:22:41 -0500

    Add files via upload

---
+++ IDE Files/ledblink.c
@@ -0,0 +1,16 @@
+#define F_CPU 8000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int
+main (void)
+{
+    DDRB |= _BV(DDB0); 
+    
+    while(1) 
+    {
+        PORTB ^= _BV(PB0);
+        _delay_ms(500);
+    }
+}
